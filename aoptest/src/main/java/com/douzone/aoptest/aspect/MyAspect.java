695112c8b39197fa7cae2d581043ed4ffd4bcb62
dntjd7701 <dntjd7701@gmail.com>
2021-06-09 11:41:42 +0900

    AOP #. Around && fin.

--- aoptest/src/main/java/com/douzone/aoptest/aspect/MyAspect.java
+++ aoptest/src/main/java/com/douzone/aoptest/aspect/MyAspect.java
@@ -1,8 +1,10 @@
 package com.douzone.aoptest.aspect;
 
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
@@ -39,6 +41,31 @@ public class MyAspect {
 	@AfterThrowing(value="execution(* *..*.*.*(..))", throwing="ex")
 	public void afterThrowingAdvice(Throwable ex) {
 		System.out.println("---After Throwing Advice : " + ex + "------");
+	}
+	
+	@Around(value="execution(* *..*.ProductService.*(..))")
+	public Object aroundAdvice(ProceedingJoinPoint pjp) throws Throwable{
+		// Before Advice
+		System.out.println("@Around(Before) Advice run...");
+			
+		// PointCut Method 실행
+		// 여기선 ProductService의 메소드인 find가 실행
+//		Object ob = pjp.proceed();
+		
+		// prameter 가로채기(바꿔치기)
+		// 남용하면 안됌. 
+		// 특수한 경우. 
+		// 공통된 부분을 지속적으로 수정 및 처리해야하는 경우
+		// 어떤게 올지 모르니, 배열로 받아야함. 
+		Object[] params = {"Camera"};
+		Object ob = pjp.proceed(params);
+		
+		// After Advice 
+		System.out.println("@Around(After) Advice run...");
+		
+		
+		// return 필수 !!
+		return ob;
 	}
 	
 	
